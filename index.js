1c4a3487fbb808cf4fe2d418682e7284ade00657
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-04-18 16:10:59 -0500

    Template Engines

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 
 var fs = require('fs');
 var _ = require('lodash');
+var engines = require('consolidate');
+
 var users = [];
 
 fs.readFile('users.json', {encoding: 'utf8'}, function(err, data) {
@@ -14,13 +16,14 @@ fs.readFile('users.json', {encoding: 'utf8'}, function(err, data) {
   });
 
 })
-app.get('/', function(req, res) {
-  var buffer = '';
 
-  users.forEach(function (user) {
-    buffer += '<a href="/' + user.username + '">' + user.name.full + '</a><br>';
-  })
-  res.send(buffer);
+app.engine('hbs', engines.handlebars);
+
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+app.get('/', function(req, res) {
+  res.render('index.jade', {users: users});
 });
 
 app.get(/big.*/, function(req, res, next) {
